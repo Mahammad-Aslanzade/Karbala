d56a4e85fee0b3aee8a01feea559deaadc87403e
Samir Israfilov <samirisrafilov2000@gmail.com>
2023-07-27 14:15:51 +0400

    hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -34,7 +34,7 @@ const HeroSection = () => {
                                 <img className='text-frame' src={text_frame} alt="" />
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).se Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. </p>
                             </div>
-                            <NavLink to="/imamhussein">Read More</NavLink>
+                            <NavLink to="/imamhussein" id='read_more'>Read More</NavLink>
                         </div>
                     </div>
                 </div>
